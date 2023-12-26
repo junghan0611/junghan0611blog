ab57db7d3a1e57c1f14545f468c42a43f5bd09fc
junghan0611 <junghanacs@gmail.com>
2023-12-26 18:20:14 +0900

    test

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,13 +1,13 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
-  language: 'en-us',
-  theme: 'system', // system, dark or light
+    title: 'Junghan0611 Text World',
+    author: 'Junghan Kim',
+    headerTitle: 'Junghan0611Blog',
+    description: 'A blog created with Next.js and Tailwind.css',
+    language: 'en-us',
+    theme: 'light', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteRepo: 'https://github.com/junghan0611/junghan0611blog',
   siteLogo: '/static/images/logo.png',
   socialBanner: '/static/images/twitter-card.png',
   mastodon: 'https://mastodon.social/@mastodonuser',
